069b7c063c013b73f61a008dbbaac888b2c0882f
jestin <jestinjois64@gmail.com>
2025-02-25 18:17:19 +0530

    added new line as delimiter

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function add(str) {
   if (str === "") return 0;
 
-  return str.split(",").reduce((acc, curr) => acc + parseInt(curr), 0);
+  return str.split(/,|\n/).reduce((acc, curr) => acc + parseInt(curr), 0);
 }
 
-console.log(add("1,2,3"));
+console.log(add("1\n2,3"));
 
 module.exports = add;
